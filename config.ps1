5428b463fad56e34af5a589bf17789a728c53514
Daniel <tinoko27@gmail.com>
2024-09-18 10:04:00 +0200

    test

---
+++ config.ps1
@@ -0,0 +1,10 @@
+# Script de PowerShell para configurar una direccion IP en un adaptor de red
+$adapterName = "Ethernet45462312"
+$ipAdress = "192.168.1.100"
+$subnetMask = "255.255.255.0
+$gateway = "192.168.1.1"
+
+# Configurar la IP estatica
+New-NetIPAddress -InterfaceAlias $adapterName -IPAddress $ipAddress -PrefixLength 24 -DefaultGateway $gateway
+# Mostrar configuracion de red actual
+Get-NetIPAddress -InterfaceAlias $adapterName
